7e5dcbbd5baa7cc5374bb31bbffcd177d8336c47
MaxiimRandob <madrandob@gmail.com>
2019-09-19 15:35:18 +0300

    task 01

---
+++ src/Task1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task1
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter your binary number: ");
+		String binNum = in.nextLine();
+
+		int decNum = 0;
+		int counter = 0;
+		for (int i = binNum.length() - 1; i >= 0; i--)
+		{
+			if (binNum.charAt(i) == '1')
+			{
+				decNum += (int)Math.pow(2, counter);
+				counter++;
+			}
+			else
+			{
+				counter++;
+			}
+		}
+
+		System.out.println(binNum + " -> " + decNum);
+	}
+}
