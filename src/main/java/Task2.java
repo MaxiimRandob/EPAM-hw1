53051eaa58edacb00b2f8e1da82bac6763a7e9a6
MaxiimRandob <madrandob@gmail.com>
2019-09-25 13:44:57 +0300

    EPAM UNIVERSITY HW commit slightly modified task1/task2 + task1Test/task2Test

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Task2
 {
 	public static void main(String[] args)
+
 	{
 		showIntegerInTheConsole(binaryNegateInt(scanFromConsole()));
 	}
--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -1,6 +1,42 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 public class Task2Test
 {
+
+
+	@Test
+	public void shouldReturnCorrectIntegerWhenConsoleInputIsValid()
+	{
+		String input = "5";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+		Assert.assertEquals(Task2.scanFromConsole(), 5);
+	}
+
+	@Test
+	public void shouldReturnZeroWhenConsoleInputIsInvalid()
+	{
+		String input = "lol";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+		Assert.assertEquals(Task2.scanFromConsole(), 0);
+	}
+
+	@Test
+	public void shouldReturnCorrectNegativeNumberWhenInputIsPositive()
+	{
+		Assert.assertEquals(Task2.binaryNegateInt(2), -2);
+	}
+
+	@Test
+	public void shouldReturnCorrectPositiveNumberWhenInputIsNegative()
+	{
+		Assert.assertEquals(Task2.binaryNegateInt(-4), 4);
+	}
 
 }
