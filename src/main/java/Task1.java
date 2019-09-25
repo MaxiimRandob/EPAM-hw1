edcae138b2a085f3261799b4d4af44e8007e239d
MaxiimRandob <madrandob@gmail.com>
2019-09-25 11:52:38 +0300

    EPAM UNIVERSITY HW commit slightly modified task1/task2 + junit

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -9,14 +9,21 @@ public class Task1
 		System.out.println("Enter your binary number: ");
 		String binNum = in.nextLine();
 
-		int decNum = convertFromBinToDec(binNum);
+		if(isValidBinNum(binNum)){
+			System.out.println("Your number is valid");
+			int decNum = convertFromBinToDec(binNum);
+			System.out.println(binNum + " -> " + decNum);
+		}
+		else {
+			System.out.println("Yor number is not valid");
+		}
 
-		System.out.println(binNum + " -> " + decNum);
 
 	}
 
 	public static int convertFromBinToDec(String binNum)
 	{
+
 		int decNum = 0;
 		int counter = 0;
 		for (int i = binNum.length() - 1; i >= 0; i--)
@@ -33,6 +40,23 @@ public class Task1
 		}
 
 		return decNum;
+	}
+
+	public static boolean isValidBinNum(String binNum)
+	{
+		if (binNum.length() == 0 || binNum.length() > Integer.SIZE)
+		{
+			return false;
+		}
+		for (int i = 0; i < binNum.length(); i++)
+		{
+			if (binNum.charAt(i) != '0' &&
+				binNum.charAt(i) != '1')
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 }
 
